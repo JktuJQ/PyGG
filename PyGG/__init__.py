434fcf7861007e310140996658620624bfbcf996
JktuJQ <odmamontov@gmail.com>
2020-12-16 08:55:30 +0300

    First PyGG commit

--- PyGG/__init__.py
+++ PyGG/__init__.py
@@ -1,3 +1,16 @@
+"""
+PyGG - simple written in Python game engine compatible with Python GUI libraries such as
+tkinter, pygame turned into wrapper classes
+
+The main purpose of this game engine - make developing 2D games on Python easier,
+applying different classes. Architecture of PyGG is similar to Unity architecture,
+it is based on GGameObjects, the functionality of which is implemented by the GComponents.
+
+---------------------------------------------------------------------------------------------------
+
+This python package was created at 15.07.20
+"""
+
 from .GErrors import *
 from .GData import *
 from .GCore import *
